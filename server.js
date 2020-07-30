cc001a84a1b591a410d6e48446fc2e05a7ebe09d
axavila <ax.avila@hotmail.com>
2020-07-29 22:24:13 -0600

    up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -16,7 +17,7 @@ const database = {
 
         },
         {
-            id: '123',
+            id: '124',
             name: 'Ruth',
             email: 'ruth@gmail.com',
             password: 'ruth',
@@ -25,6 +26,13 @@ const database = {
 
         }
 
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'ax@gmail.com'
+        }
     ]
 }
 
@@ -33,6 +41,14 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/signin', (req, res)=> {
+
+    bcrypt.compare("mery", '$2a$10$Qw8jA90cRxbOLFBryjL1au49nLqO4RuH700AX1yBduEEhOnks/x7S', function(err, res) {
+            console.log('first guess', res);
+        // });
+
+    bcrypt.compare("bacon", '$2a$10$Qw8jA90cRxbOLFBryjL1au49nLqO4RuH700AX1yBduEEhOnks/x7S', function(err, res) {
+        console.log('second guess', res);
+            // });
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
         res.json('success');
     } else {
@@ -42,6 +58,7 @@ app.post('/signin', (req, res)=> {
 
 app.post('/register', (req, res) => {
     const {email, name, password } = req.body;
+
     database.users.push({
         id: '125',
         name: name,
@@ -53,6 +70,52 @@ app.post('/register', (req, res) => {
     })
     res.json(database.users[database.users.length-1]);
 })
+
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found =false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            res.json(user);
+        }
+    })
+
+    if(!found) {
+        res.status(400).json('not found');
+    }
+
+})
+
+app.put('/image', (req, res) => {
+    const {id} = req.body;
+    let found =false;
+
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            res.json(user.entries);
+        }
+    })
+    if(!found) {
+        res.status(400).json('not found');
+    }
+})
+
+
+
+
+// Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
+
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
